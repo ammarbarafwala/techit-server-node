87df12ac36c44ac2deaf4e35cc65c68e33e13a53
Ammar <ammarbarafwala@gmail.com>
2018-05-03 16:26:42 -0700

    Added User model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    hash: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true
+    }
+})
